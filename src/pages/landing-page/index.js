95d6505a4183645c3f058645e6a73e0ebcee7f6b
jorius <josedrc97@gmail.com>
2020-08-29 00:58:04 -0500

    Update landing page

--- src/pages/landing-page/index.js
+++ src/pages/landing-page/index.js
@@ -1,21 +1,125 @@
 // @packages
+import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
+
+// @scripts
+import { config } from '../../config';
 
 // @styles
 import styles from './styles';
 
-const LandingPage = ({ classes, content }) =>
-    (
-        <div id="home-page" className={classes.landingPage}>
-            {content}
-        </div>
+// @images
+import IcIlustraHero from '../../assets/images/Ic_ilustra_Hero.png';
+import logo from '../../assets/images/logo_full_color.svg';
+
+const LandingPage = ({ classes }) => {
+    const renderMainMenu = (
+        <>
+            <Grid
+                alignItems="center"
+                className={classes.menu}
+                container
+                direction="row"
+                justify="center"
+            >
+                <Grid item>
+                    <img alt="logo" src={logo} style={{ width: 220 }} />
+                </Grid>
+                <Grid item>
+                    <Typography
+                        className={classes.menuItem}
+                        variant="body1"
+                    >
+                        {config.text.landingPage.start}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography
+                        className={classes.menuItem}
+                        variant="body1"
+                    >
+                        {config.text.landingPage.technologies}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography
+                        className={classes.menuItem}
+                        variant="body1"
+                    >
+                        {config.text.landingPage.benefits}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography
+                        className={classes.menuItem}
+                        variant="body1"
+                    >
+                        {config.text.landingPage.requirements}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Button
+                        classes={{ root: classes.loginButton }}
+                        color="primary"
+                        id="login-button"
+                        size="large"
+                        value={config.text.loginPage.continue}
+                    >
+                        {config.text.loginPage.formTitle}
+                    </Button>
+                </Grid>
+            </Grid>
+        </>
     );
 
+    return (
+        <div id="landing-page" className={classes.landingPage}>
+            {renderMainMenu}
+            <Grid
+                alignItems="center"
+                container
+                direction="row"
+                justify="center"
+            >
+                <Grid className={classes.welcomeSentenceContainer} item lg={6} md={6} sm={6} xs={12}>
+                    <div className={classes.welcomeSentence}>
+                        <Typography
+                            className={classes.mainText}
+                            variant="h3"
+                        >
+                            {config.text.landingPage.welcomeToYour}
+                        </Typography>
+                        <Typography
+                            className={classes.mainText}
+
+                            style={{ fontWeight: 'bold' }}
+                            variant="h3"
+                        >
+                            {config.text.landingPage.technicalInterview}
+                        </Typography>
+                        <Typography
+                            className={classes.mainText}
+                            style={{ color: '#97CF00', fontWeight: 'bold' }}
+                            variant="h3"
+                        >
+                            {config.text.appName}
+                        </Typography>
+                    </div>
+                </Grid>
+                <Grid className={classes.icIlustraHeroImage} item lg={6} md={6} sm={6} xs={12}>
+                    <img alt="" src={IcIlustraHero} />
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+
 LandingPage.propTypes = {
-    classes: PropTypes.object.isRequired,
-    content: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired
+    classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(LandingPage);
--- src/pages/landing-page/styles.js
+++ src/pages/landing-page/styles.js
@@ -1,8 +1,45 @@
+// @scripts
+import { globalUI } from '../../core';
+
 // @styles
 import globals from '../../styles/globals';
 
 export default (theme) => ({
     ...globals(theme),
+    icIlustraHeroImage: {
+        textAlign: 'center',
+        transform: 'scaleX(-1)'
+    },
     landingPage: {
+        background: `url("${globalUI.getImage('./Bg_Header.png')}") repeat`,
+        height: '100vh'
+    },
+    loginButton: {
+        fontSize: '24px',
+        fontWeight: 'bold',
+        width: 100
+    },
+    mainText: {
+        textAlign: 'left'
+    },
+    menu: {
+    },
+    menuItem: {
+        borderBottom: '2px solid transparent',
+        color: theme.palette.common.black,
+        cursor: 'pointer',
+        fontSize: '24px',
+        fontWeight: 'bold',
+        padding: 25,
+        '&:hover': {
+            borderBottom: `2px solid ${theme.palette.common.black}`
+        }
+    },
+    welcomeSentenceContainer: {
+        width: '100%'
+    },
+    welcomeSentence: {
+        margin: '0 auto',
+        width: '55%'
     }
 });
