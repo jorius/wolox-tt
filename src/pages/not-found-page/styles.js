ef0a09874491b00072242ef91bb31fa8b6e68b49
jorius <josedrc97@gmail.com>
2020-09-07 09:20:14 -0500

    Improve the the style for 404 page

--- src/pages/not-found-page/styles.js
+++ src/pages/not-found-page/styles.js
@@ -1,7 +1,12 @@
+// @scripts
 import globals from '../../styles/globals';
 
 export default (theme) => ({
     ...globals(theme),
     notFoundPage: {
+        left: '50%',
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(-50%, -50%)'
     }
 });
